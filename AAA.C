2806b4ba7d831bd0d02af695f7be1cf440ef89a2
PRAVEENSV777 <31157729+PRAVEENSV777@users.noreply.github.com>
2017-08-29 16:28:13 +0530

    Create AAA.C

---
+++ AAA.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("ENTER AN INTEGER');
+scanf("%d",&NUMBER);
+// TRUE IF THE NUMBER IS PERFECTLY DIVISIBLE BY 2
+if(number%2==0)
+printf("%d is even.",number);
+else
+printf(
